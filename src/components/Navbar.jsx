141ef01bfea9e89522d2ba7f908066c2bdc5bf4c
sebastianhoyoslopez <tatansebastan@gmail.com>
2021-03-06 13:41:49 -0500

    login with email and logout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,46 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { signOut } from "../redux/UsuarioDucks";
-import {withRouter} from 'react-router-dom'
+import { withRouter } from "react-router-dom";
 
 const Navbar = (props) => {
   const dispatch = useDispatch();
 
   const SignO = () => {
     dispatch(signOut());
-    props.history.push('/login')
+    props.history.push("/login");
   };
+
+  const activo = useSelector((store) => store.usuario.activo);
+  const user = useSelector((store) => store.usuario.user);
+
   return (
     <div className="navbar navbar-dark bg-dark">
-      <Link className="navbar-brand mx-4" to="/">
-        Redux Challenge
-      </Link>
-      <div className="d-flex">
-        <NavLink className="btn btn-dark mr-2" to="/" exact>
-          Inicio
-        </NavLink>
+      {activo ? (
+        <Link className="navbar-brand mx-4" to="/">
+          {user.nombre}
+        </Link>
+      ) : (
+        <Link className="navbar-brand mx-4" to="/">
+          Redux Challenge
+        </Link>
+      )}
+      {activo ? (
+        <div className="d-flex">
+          <NavLink className="btn btn-dark mr-2" to="/" exact>
+            Inicio
+          </NavLink>
+          <button className="btn btn-dark mr-2" onClick={() => SignO()}>
+            Salir
+          </button>
+        </div>
+      ) : (
         <NavLink className="btn btn-dark mr-2" to="/login" exact>
           Login
         </NavLink>
-        <button className="btn btn-dark mr-2" onClick={() => SignO()}>
-          Salir
-        </button>
-      </div>
+      )}
     </div>
   );
 };
